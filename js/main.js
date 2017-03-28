670adb21fb7b6d0e211a3a55fb47faae4a8ddd4c
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-03-28 21:28:19 +0300

    Add start menu, buttons, score table, separate start method

--- js/main.js
+++ js/main.js
@@ -1,96 +1,22 @@
 window.addEventListener('load', function () {
-    'use strict';
-    const WIDTH = 800,
-        HEIGHT = 500,
-        loopsPerTick = 7,
-        harrySpritePerRow = 3,
-        harrySpriteRows = 4,
-        rowWalkUp = 0,
-        rowWalkDown = 1,
-        rowWalkRight = 2,
-        rowWalkLeft = 3;
+    mainMenu();
+});
 
-    createBackground({ width: WIDTH + 60, height: HEIGHT + 40 });
+var main = document.getElementById('main');
 
-    const playerCanvas = document.getElementById('player-canvas'),
-        playerContext = playerCanvas.getContext('2d'),
-        playerImg = document.getElementById('harry-sprite');
+function hide(element) {
+  element.style.display = 'none';
+};
 
+function show(element) {
+  element.style.display = 'block';
+} ;
 
-    playerCanvas.width = WIDTH;
-    playerCanvas.height = HEIGHT;
+function mainMenu() {
+  show(main);
+};
 
-    var harrySprite = createSprite({
-        sprite: playerImg,
-        context: playerContext,
-        width: playerImg.width / harrySpritePerRow,
-        height: playerImg.height / harrySpriteRows,
-        rowNumber: 0,
-        numberOfFrames: 0,
-        loopTicksPerFrame: loopsPerTick
-    });
-
-    const harrySpeed = 2,
-        harryInitialX = (WIDTH / 2) - (harrySprite.width / 2),
-        harryInitialY = HEIGHT - harrySprite.height;
-
-    var harryBody = createPhysicalBody({
-        coordinates: { x: harryInitialX, y: harryInitialY },
-        speed: { x: 0, y: 0 },
-        width: harrySprite.width,
-        height: harrySprite.height
-    });
-
-    window.addEventListener('keydown', function (event) {
-
-        switch (event.keyCode) {
-            case 37:
-                if (harryBody.speed.x < 0) {
-                    return;
-                }
-                harryBody.speed.x = -harrySpeed;
-                harrySprite.rowNumber = rowWalkLeft;
-                harrySprite.numberOfFrames = harrySpritePerRow;
-                break;
-            case 38:
-                harryBody.speed.y = -harrySpeed;
-                harrySprite.rowNumber = rowWalkUp;
-                harrySprite.numberOfFrames = harrySpritePerRow;
-                break;
-            case 39:
-                harryBody.speed.x = harrySpeed;
-                harrySprite.rowNumber = rowWalkRight;
-                harrySprite.numberOfFrames = harrySpritePerRow;
-                break;
-            case 40:
-                harryBody.speed.y = harrySpeed;
-                harrySprite.rowNumber = rowWalkDown;
-                harrySprite.numberOfFrames = harrySpritePerRow;
-                break;
-            default:
-                break;
-        }
-    });
-
-    window.addEventListener('keyup', function (event) {
-        if ((event.keyCode < 37) && (event.keyCode > 40)) {
-            return;
-        }
-
-        harryBody.speed.x = 0;
-        harryBody.speed.y = 0;
-        harrySprite.numberOfFrames = 0;
-    });
-
-    function gameLoop() {
-
-        var lastHarryCoordinates = harryBody.move({ x: WIDTH - harrySprite.width, y: HEIGHT - harrySprite.height });
-
-
-        harrySprite.render(lastHarryCoordinates, harryBody.coordinates).update();
-
-        window.requestAnimationFrame(gameLoop);
-    }
-
-    gameLoop();
+document.querySelectorAll('.play')[0].addEventListener('click', function() {
+  hide(main);
+  start();
 });
