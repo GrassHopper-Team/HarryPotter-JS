2c25a2648bfdd7a7d671d9a9851a11b0bef539c2
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-03-29 12:46:49 +0300

    Added highscore, end game with submit for score and name

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ window.addEventListener('load', function () {
 });
 
 var main = document.querySelector('#main');
+var highScore = document.getElementById('high-score');
+var topScore = document.getElementById('top-scores');
 var creditsInfo = document.querySelector('#creditsWrapper');
+var endScreen = document.querySelector('.end-screen');
 
 var playBtn = document.querySelector('#playBtn');
 var scoreBtn = document.querySelector('#scoreBtn');
 var creditsBtn = document.querySelector('#creditsBtn');
+var backBtn = document.querySelector('.high-score .button');
+var submitBtn = document.querySelectorAll('.end-screen .submit-button')[0];
 
 function hide(element) {
   element.style.display = 'none';
@@ -27,10 +32,27 @@ playBtn.addEventListener('click', function () {
   start();
 });
 
+scoreBtn.addEventListener('click', function () {
+  hide(main);
+  //Not finished working on it! (sorting top scores)
+  show(highScore);
+});
+
+backBtn.addEventListener('click', function(){
+  hide(highScore);
+  show(main);
+});
+
 creditsBtn.addEventListener('click', function () {
   hide(main);
   show(creditsInfo);
-})
+});
+
+submitBtn.addEventListener('click', function(){
+  hide(endScreen);
+  //Almost done!(storing the score and name)
+  show(main);
+});
 
 window.onkeydown = function (event) {
   // Pressing ESC should hide credits/score and show the menu
